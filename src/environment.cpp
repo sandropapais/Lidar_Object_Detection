5ba06506b26dad01c800564cb827c752580824a6
Sandro Papais <papais.sandro@gmail.com>
2021-03-16 01:24:26 +0000

    added pcl plane RANSAC

--- src/environment.cpp
+++ src/environment.cpp
@@ -54,8 +54,10 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     renderPointCloud(viewer, inputCloud, "inputCloud");
 
     // Create point processor
-    //ProcessPointClouds<pcl::PointXYZ> pointProcessor;
-  
+    ProcessPointClouds<pcl::PointXYZ> pointProcessor; // on the stack
+    std::pair<pcl::PointCloud<pcl::PointXYZ>::Ptr, pcl::PointCloud<pcl::PointXYZ>::Ptr> segmentCloud = pointProcessor.SegmentPlane(inputCloud, 100, 0.2);
+    renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1,0,0));
+    renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0,1,0));
 }
 
 
