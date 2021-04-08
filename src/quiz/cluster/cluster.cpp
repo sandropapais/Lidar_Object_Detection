dd669802c5be4f0e5b153044836443b3eb30eca4
Sandro Papais <papais.sandro@gmail.com>
2021-04-08 03:58:18 +0000

    finished recursive 2D cluster function

--- src/quiz/cluster/cluster.cpp
+++ src/quiz/cluster/cluster.cpp
@@ -75,12 +75,49 @@ void render2DTree(Node* node, pcl::visualization::PCLVisualizer::Ptr& viewer, Bo
 
 }
 
+void clusterHelper(int index, const std::vector<std::vector<float>> points, std::vector<int>& cluster, std::vector<bool>& processed, KdTree* tree, float distanceTol)
+{
+	processed[index] = true;
+	cluster.push_back(index);
+	
+	// find nearest points within distance tolerance
+	std::vector<int> nearest = tree->search(points[index], distanceTol);
+
+	// for each nearby point call cluster helper again if not processed
+	for(int id : nearest)
+	{
+		if(!processed[id])
+		{
+			clusterHelper(id, points, cluster, processed, tree, distanceTol);
+		}
+	}
+
+}
+
 std::vector<std::vector<int>> euclideanCluster(const std::vector<std::vector<float>>& points, KdTree* tree, float distanceTol)
 {
 
-	// TODO: Fill out this function to return list of indices for each cluster
+	// return list of indices for each cluster
 
 	std::vector<std::vector<int>> clusters;
+	std::vector<bool> processed(points.size(),false);
+
+	// iterate through all points
+	int i = 0;
+	while(i < points.size())
+	{
+		if(processed[i])
+		{
+			i++;
+			continue;
+		}
+
+		// if not processed create a cluster and call helper
+		std::vector<int> cluster;
+		clusterHelper(i, points, cluster, processed, tree, distanceTol);
+		clusters.push_back(cluster);
+		i++;
+	}
  
 	return clusters;
 
