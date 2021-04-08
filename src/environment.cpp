a01b478913919018a17c6bae38e512c3c0f51e66
Sandro Papais <papais.sandro@gmail.com>
2021-04-08 04:14:04 +0000

    completed clustering and bounding box render

--- src/environment.cpp
+++ src/environment.cpp
@@ -43,6 +43,10 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     
     // RENDER OPTIONS
     bool renderScene = false;
+    bool render_obst = true;
+    bool render_plane = true;
+    bool render_clusters = true;
+    bool render_box = true;
     std::vector<Car> cars = initHighway(renderScene, viewer);
     
     // Create lidar sensor 
@@ -56,23 +60,39 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     // Create point processor
     ProcessPointClouds<pcl::PointXYZ> pointProcessor; // on the stack
     std::pair<pcl::PointCloud<pcl::PointXYZ>::Ptr, pcl::PointCloud<pcl::PointXYZ>::Ptr> segmentCloud = pointProcessor.SegmentPlane(inputCloud, 100, 0.2);
-    renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1,0,0));
-    renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0,1,0));
+    if(render_obst)
+    {
+        renderPointCloud(viewer, segmentCloud.first, "obstCloud", Color(1,0,0));
+    }
+    if(render_plane)
+    {
+        renderPointCloud(viewer, segmentCloud.second, "planeCloud", Color(0,1,0));
+    }
 
     std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> cloudClusters = pointProcessor.Clustering(segmentCloud.first, 1.0, 3, 30);
 
     int clusterId = 0;
     std::vector<Color> colors = {Color(1,0,0), Color(1,1,0), Color(0,0,1)};
+    
     for(pcl::PointCloud<pcl::PointXYZ>::Ptr cluster : cloudClusters)
     {
-        std::cout << "cluster size ";
-        pointProcessor.numPoints(cluster);
-        renderPointCloud(viewer, cluster, "obstCloud"+std::to_string(clusterId), colors[clusterId%colors.size()]);
-    
+        if(render_clusters)
+        {
+            std::cout << "cluster size ";
+            pointProcessor.numPoints(cluster);
+            renderPointCloud(viewer, cluster, "obstCloud"+std::to_string(clusterId), colors[clusterId%colors.size()]);
+        }
+        if(render_box)
+        {
+            Box box = pointProcessor.BoundingBox(cluster);
+            renderBox(viewer,box,clusterId);
+        }
+
         ++clusterId;
 
     }
 
+    //renderPointCloud(viewer, segmentCloud.second, "planeCloud");
 
 }
 
